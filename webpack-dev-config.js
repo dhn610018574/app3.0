3273b66e40feb223e3f8a158b3ae93e41e6ca688
hainan_dong <hainan_dong@crfchina.com>
2017-12-27 18:03:51 +0800

    实现webpack热启动

--- webpack-dev-config.js
+++ webpack-dev-config.js
@@ -14,15 +14,28 @@ module.exports = {
     filename: '[name].js'
   },
   module: {
-    rules:[
-      {
-        test:/\.jsx?$/,
-        exclude:/node_modules/,
-        use:[{
-          loader: "babel-loader"
-        }]
-      
-      }
-    ]
-  }
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: [{
+        loader: "babel-loader"
+      }]
+
+    }]
+  },
+  //webpack-dev-server配置
+  devServer: {
+    contentBase: './dist', //默认webpack-dev-server会为根文件夹提供本地服务器，如果想为另外一个目录下的文件提供本地服务器，应该在这里设置其所在目录（本例设置到"build"目录）
+    // colors: true, //在cmd终端中输出彩色日志
+    historyApiFallback: true, //在开发单页应用时非常有用，它依赖于HTML5 history API，如果设置为true，所有的跳转将指向index.html
+    host: '10.194.11.67',
+    port: 8080, //设置默认监听端口，如果省略，默认为"8080"
+    hot: true, //是否启动热更新
+    open: true, //是否支持自动打开浏览器
+    // inline: true, //设置为true，当源文件改变时会自动刷新页面
+    // process: true, //显示合并代码进度
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin() //热模块替换插件
+  ]
 }
