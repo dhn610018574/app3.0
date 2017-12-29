7cec59bb38a5e072a216f3848cb02f46dcbfd80a
hainan_dong <hainan_dong@crfchina.com>
2017-12-29 15:17:27 +0800

    成功处理react-router的大坑！！

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Routers from './js/Routers.jsx';
+import {Router,Route,IndexRoute, Link,browserHistory} from 'react-router'
 ReactDom.render(
-    <div>
-        <Routers />
-    </div>,
+  <Router history={browserHistory} routes={Routers} />,
     document.getElementById('app')
 );
--- src/js/Routers.jsx
+++ src/js/Routers.jsx
@@ -1,22 +1,22 @@
 import React ,{Component} from 'react'
-import {
-  Router,
-  Route,
-  IndexRoute, 
-  Link,
-  browserHistory
-} from 'react-router'
+import {Router,Route,IndexRoute, Link} from 'react-router'
 import {
   App
 }  from './components'
-export default class Routers extends Component {
-  render () {
-    return (
-      <Router history={browserHistory} >
-        <Route path='/' component={App}></Route>
+// export default class Routers extends Component {
+//   render () {
+//     return (
+//       <Router history={browserHistory} >
+//         <Route path='/' component={App}></Route>
 
-      </Router>
-    )
-  }
-}
+//       </Router>
+//     )
+//   }
+// }
 
+export default (
+  <Router path='/' component={App}>
+    <Route path='home' component={App}></Route>
+
+  </Router>
+)
